02c6236e948bcb8ff4b08dbd48795cc77d419200
Richard L. Jackson <denezt@yahoo.com>
2019-02-19 06:10:17 +0100

    Modifying, refactor script

--- refactor.sh
+++ refactor.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 
-/usr/local/bin/jscodeshift -t signature-change.js signature-change.input.js -d -p
+mkdir -v Backups
+if [[ -d "Backups" ]]; then
+	#statements
+	cp -a -v "signature-change.input.js" Backups/
+	/usr/local/bin/jscodeshift -t signature-change.js signature-change.input.js -d -p
+else 
+	#statements
+	printf "\033[31mmissing Backups directory\033[0m\n"
+fi
